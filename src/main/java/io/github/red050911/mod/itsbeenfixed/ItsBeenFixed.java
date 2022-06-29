4818f99100f781445fedc25cc0a3171cb4d968d3
Jacob Rutledge <jacob.rutledge0509@gmail.com>
2022-06-29 12:15:00 -0500

    v1.1

--- src/main/java/io/github/red050911/mod/itsbeenfixed/ItsBeenFixed.java
+++ src/main/java/io/github/red050911/mod/itsbeenfixed/ItsBeenFixed.java
@@ -17,6 +17,9 @@ public class ItsBeenFixed implements ModInitializer {
     public static GameRules.Key<GameRules.BooleanRule> CACTI_BREAK_ITEMS_RULE = null;
     public static GameRules.Key<GameRules.BooleanRule> CACTI_BREAK_MINECARTS_RULE = null;
     public static GameRules.Key<GameRules.BooleanRule> CACTI_BREAK_BOATS_RULE = null;
+    public static GameRules.Key<GameRules.BooleanRule> WATER_CAULDRONS_BREAK_FALL = null;
+    public static GameRules.Key<GameRules.BooleanRule> ENTITIES_CONSIDERED_WET_IN_CAULDRONS = null;
+    public static GameRules.Key<GameRules.BooleanRule> CAULDRONS_ALLOW_NETHER_WATER = null;
 
     @Override
     public void onInitialize() {
@@ -28,6 +31,9 @@ public class ItsBeenFixed implements ModInitializer {
         CACTI_BREAK_ITEMS_RULE = GameRuleRegistry.register("cactiBreakItems", FIXES_RULE_CATEGORY, GameRuleFactory.createBooleanRule(false));
         CACTI_BREAK_MINECARTS_RULE = GameRuleRegistry.register("cactiBreakMinecarts", FIXES_RULE_CATEGORY, GameRuleFactory.createBooleanRule(false));
         CACTI_BREAK_BOATS_RULE = GameRuleRegistry.register("cactiBreakBoats", FIXES_RULE_CATEGORY, GameRuleFactory.createBooleanRule(false));
+        WATER_CAULDRONS_BREAK_FALL = GameRuleRegistry.register("waterCauldronsBreakFall", FIXES_RULE_CATEGORY, GameRuleFactory.createBooleanRule(true));
+        ENTITIES_CONSIDERED_WET_IN_CAULDRONS = GameRuleRegistry.register("entitiesAreWetInWaterCauldrons", FIXES_RULE_CATEGORY, GameRuleFactory.createBooleanRule(true));
+        CAULDRONS_ALLOW_NETHER_WATER = GameRuleRegistry.register("cauldronsAllowNetherWater", FIXES_RULE_CATEGORY, GameRuleFactory.createBooleanRule(false));
     }
 
 }
---
+++ src/main/java/io/github/red050911/mod/itsbeenfixed/mixin/MixinCauldronBehavior.java
@@ -0,0 +1,41 @@
+package io.github.red050911.mod.itsbeenfixed.mixin;
+
+import io.github.red050911.mod.itsbeenfixed.ItsBeenFixed;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.cauldron.CauldronBehavior;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(CauldronBehavior.class)
+public interface MixinCauldronBehavior {
+
+    @Inject(at = @At("RETURN"), method = "fillCauldron")
+    private static void fillCauldron(World world, BlockPos pos, PlayerEntity player, Hand hand, ItemStack stack, BlockState state, SoundEvent soundEvent, CallbackInfoReturnable<ActionResult> cir) {
+        if(!world.isClient) {
+            if(world.getDimension().isUltrawarm() && state.getBlock().equals(Blocks.WATER_CAULDRON) && !world.getGameRules().getBoolean(ItsBeenFixed.CAULDRONS_ALLOW_NETHER_WATER)) {
+                world.setBlockState(pos, Blocks.CAULDRON.getDefaultState());
+                world.playSound(null, pos, SoundEvents.BLOCK_FIRE_EXTINGUISH, SoundCategory.BLOCKS, 1.0f, 1.0f);
+                int x = pos.getX();
+                int y = pos.getY() + 1;
+                int z = pos.getZ();
+                for (int l = 0; l < 8; ++l) {
+                    world.addParticle(ParticleTypes.LARGE_SMOKE, (double)x + Math.random(), (double)y + Math.random(), (double)z + Math.random(), 0.0, 0.0, 0.0);
+                }
+            }
+        }
+    }
+
+}
