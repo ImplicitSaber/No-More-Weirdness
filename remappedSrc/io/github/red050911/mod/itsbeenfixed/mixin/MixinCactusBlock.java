9e5c9bd63a7fd95168eef4c8524c46006c95731f
Jacob Rutledge <jacob.rutledge0509@gmail.com>
2022-06-29 12:17:13 -0500

    v1.1.0

---
+++ remappedSrc/io/github/red050911/mod/itsbeenfixed/mixin/MixinCactusBlock.java
@@ -0,0 +1,35 @@
+package io.github.red050911.mod.itsbeenfixed.mixin;
+
+import io.github.red050911.mod.itsbeenfixed.ItsBeenFixed;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.CactusBlock;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.ItemEntity;
+import net.minecraft.entity.vehicle.AbstractMinecartEntity;
+import net.minecraft.entity.vehicle.BoatEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(CactusBlock.class)
+public class MixinCactusBlock {
+
+    @Inject(method = "onEntityCollision", at = @At("HEAD"), cancellable = true)
+    private void injectEntityCollisionHead(BlockState state, World world, BlockPos pos, Entity entity, CallbackInfo info) {
+        if(entity instanceof ItemEntity && !world.getGameRules().getBoolean(ItsBeenFixed.CACTI_BREAK_ITEMS_RULE)) {
+            info.cancel();
+            return;
+        }
+        if(entity instanceof BoatEntity && !world.getGameRules().getBoolean(ItsBeenFixed.CACTI_BREAK_BOATS_RULE)) {
+            info.cancel();
+            return;
+        }
+        if(entity instanceof AbstractMinecartEntity && !world.getGameRules().getBoolean(ItsBeenFixed.CACTI_BREAK_MINECARTS_RULE)) {
+            info.cancel();
+        }
+    }
+
+}
